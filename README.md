2986a265930fc8b797bf8944e675be47458949e1
Jonas Myhr Refseth <jonas@oisann.net>
2018-05-20 18:08:41 +0200

    v1.4

---
+++ README.md
@@ -0,0 +1,5 @@
+# User-Data
+```
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/Oisann/droplet-user-data/master/user-data.sh | sh
+```
--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install fail2ban -y
-
 sudo fallocate -l 5G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -11,4 +7,9 @@ sudo swapon /swapfile
 echo "/swapfile   none    swap    sw    0   0" | sudo tee -a /etc/fstab
 echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
 echo "vm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf
+
+sudo apt-get update
+sudo apt-get install fail2ban -y
+yes | sudo apt-get upgrade -y
+
 sudo shutdown -r now
